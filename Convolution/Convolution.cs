9cda8a658c6343c6120475d516ba8006e68f262c
Vladimir Untura <vladimir.untura@gmail.com>
2012-11-10 12:18:36 +0200

    Добавлена заготовка класса Convolution

---
+++ Convolution/Convolution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Convolution
+{
+    public class Convolution
+    {
+        double [,] filter;
+        
+        public Convolution(double[,] filter)
+        {
+            this.filter = filter;
+        }
+
+        public Convolution()
+        {
+            filter = new double[,]{
+                                {1, 2, 1},
+                                {2, 4, 2},
+                                {1, 2, 1}
+                                };
+        }
+
+        public double[,] Apply(double[,] image)
+        {
+            return null;
+        }
+    }
+}
