f7f3089ce19d383d848313bc3380b992eb8bb2a5
Vladimir Untura <vladimir.untura@gmail.com>
2012-11-30 23:43:49 +0200

    Добавлены методы для одном. и двум. обратных БПФ

--- Fourier/FFT.cs
+++ Fourier/FFT.cs
@@ -36,13 +36,13 @@ namespace Fourier
             for (int u = 0; u < K; u++)
                 for (int x = 0; x < K; x++)
                 {
-                    F_even[u] += 1 / K * a[2 * x] * W(K, u, x);
-                    F_odd[u]  += 1 / K * a[2 * x + 1] * W(K, u, x);
+                    F_even[u] += 1.0 / K * a[2 * x] * W(K, u, x);
+                    F_odd[u]  += 1.0 / K * a[2 * x + 1] * W(K, u, x);
                 }
             for (int u = 0; u < K; u++)
             {
-                F[u]     = 1 / 2 * (F_even[u] + F_odd[u] * W(2 * K, u, 1));
-                F[u + K] = 1 / 2 * (F_even[u] - F_odd[u] * W(2 * K, u, 1));
+                F[u]     = 0.5 * (F_even[u] + F_odd[u] * W(2 * K, u, 1));
+                F[u + K] = 0.5 * (F_even[u] - F_odd[u] * W(2 * K, u, 1));
             }
 
             return F;
@@ -70,15 +70,15 @@ namespace Fourier
                 {
                     for (int x = 0; x < K_line; x++)
                     {
-                        F_even[i, j] += 1 / K_line * a[i, 2 * x]     * W(K_line, i, x);
-                        F_odd[i, j]  += 1 / K_line * a[i, 2 * x + 1] * W(K_line, j, x);
+                        F_even[i, j] += 1.0 / K_line * a[i, 2 * x]     * W(K_line, i, x);
+                        F_odd[i, j]  += 1.0 / K_line * a[i, 2 * x + 1] * W(K_line, j, x);
                     }
                 }
 
                 for (int j = 0; j < K_line; j++)
                 {
-                    F[i, j]          = 1 / 2 * (F_even[i, j] + F_odd[i, j] * W(2 * K_line, j, 1));
-                    F[i, j + K_line] = 1 / 2 * (F_even[i, j] - F_odd[i, j] * W(2 * K_line, j, 1));
+                    F[i, j]          = 0.5 * (F_even[i, j] + F_odd[i, j] * W(2 * K_line, j, 1));
+                    F[i, j + K_line] = 0.5 * (F_even[i, j] - F_odd[i, j] * W(2 * K_line, j, 1));
                 }
             }
 
@@ -89,19 +89,101 @@ namespace Fourier
                 {
                     for (int x = 0; x < K_col; x++)
                     {
-                        F_even[i, j] += 1 / K_col * a[2 * x,     j] * W(K_col, i, x);
-                        F_odd[i, j]  += 1 / K_col * a[2 * x + 1, j] * W(K_col, i, x);
+                        F_even[i, j] += 1.0 / K_col * a[2 * x,     j] * W(K_col, i, x);
+                        F_odd[i, j]  += 1.0 / K_col * a[2 * x + 1, j] * W(K_col, i, x);
                     }
                 }
 
                 for (int i = 0; i < K_col; i++)
                 {
-                    F[i, j]         = 1 / 2 * (F_even[i, j] + F_odd[i, j] * W(2 * K_col, j, 1));
-                    F[i + K_col, j] = 1 / 2 * (F_even[i, j] - F_odd[i, j] * W(2 * K_col, j, 1));
+                    F[i, j]         = 0.5 * (F_even[i, j] + F_odd[i, j] * W(2 * K_col, j, 1));
+                    F[i + K_col, j] = 0.5 * (F_even[i, j] - F_odd[i, j] * W(2 * K_col, j, 1));
                 }
             }
 
             return F;
+        }
+
+        /// <summary>
+        /// Одномерное обратное быстрое преобразование Фурье
+        /// </summary>
+        /// <param name="A">Входной массив с Фурье-образом</param>
+        /// <returns></returns>
+        static public Complex[] IFFT1D(Complex[] A)
+        {
+            int K = A.Length / 2;
+            Complex[] f = new Complex[2 * K];
+            Complex[] f_even = new Complex[2 * K];
+            Complex[] f_odd = new Complex[2 * K];
+
+            for (int u = 0; u < K; u++)
+                for (int x = 0; x < K; x++)
+                {
+                    f_even[u] += 1.0 / K * A[2 * x]     * W(K, -u, x);
+                    f_odd[u]  += 1.0 / K * A[2 * x + 1] * W(K, -u, x);
+                }
+            for (int u = 0; u < K; u++)
+            {
+                f[u]     = 0.5 * (f_even[u] + f_odd[u] * W(2 * K, -u, 1));
+                f[u + K] = 0.5 * (f_even[u] - f_odd[u] * W(2 * K, -u, 1));
+            }
+
+            return f;
+        }
+
+        /// <summary>
+        /// Двумерное обратное быстрое преобразование Фурье
+        /// </summary>
+        /// <param name="a">Входной массив с Фурье-образом</param>
+        /// <returns></returns>
+        static public Complex[,] IFFT2D(Complex[,] a)
+        {
+            int K_line = a.GetLength(1) / 2;
+            int K_col = a.GetLength(0) / 2;
+
+            Complex[,] f = new Complex[a.GetLength(0), a.GetLength(1)];
+            Complex[,] f_even = new Complex[2 * K_line, 2 * K_col];
+            Complex[,] f_odd = new Complex[2 * K_line, 2 * K_col];
+
+            //Применение одномерного обратного БПФ по строкам
+            for (int i = 0; i < 2 * K_line; i++)
+            {
+                for (int j = 0; j < 2 * K_col; j++)
+                {
+                    for (int x = 0; x < K_line; x++)
+                    {
+                        f_even[i, j] += 1.0 / K_line * a[i, 2 * x]     * W(K_line, -i, x);
+                        f_odd[i, j]  += 1.0 / K_line * a[i, 2 * x + 1] * W(K_line, -j, x);
+                    }
+                }
+
+                for (int j = 0; j < K_line; j++)
+                {
+                    f[i, j]          = 0.5 * (f_even[i, j] + f_odd[i, j] * W(2 * K_line, -j, 1));
+                    f[i, j + K_line] = 0.5 * (f_even[i, j] - f_odd[i, j] * W(2 * K_line, -j, 1));
+                }
+            }
+
+            //Применение одномерного обратного БПФ по столбцам
+            for (int j = 0; j < 2 * K_col; j++)
+            {
+                for (int i = 0; i < 2 * K_line; i++)
+                {
+                    for (int x = 0; x < K_col; x++)
+                    {
+                        f_even[i, j] += 1.0 / K_col * a[2 * x, j] *     W(K_col, -i, x);
+                        f_odd[i, j]  += 1.0 / K_col * a[2 * x + 1, j] * W(K_col, -i, x);
+                    }
+                }
+
+                for (int i = 0; i < K_col; i++)
+                {
+                    f[i, j]         = 0.5 * (f_even[i, j] + f_odd[i, j] * W(2 * K_col, -j, 1));
+                    f[i + K_col, j] = 0.5 * (f_even[i, j] - f_odd[i, j] * W(2 * K_col, -j, 1));
+                }
+            }
+
+            return f;
         }
     }
 }
